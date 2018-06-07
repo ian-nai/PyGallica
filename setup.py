fa48b31140a0db871036179cdba8aba08acc16db
Ian <iangoodale@utexas.edu>
2018-06-07 14:58:28 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name = 'pygallica',
+    packages = ['pygallica'], 
+    version = '0.1',
+    description = 'A Python client for the Gallica API',
+    author = 'Ian Goodale',
+    author_email = 'ianpgoodale@gmail.com',
+    url = 'https://github.com/ian-nai',   
+    download_url = 'https://github.com/ian-nai/PyGallica/archive/master.tar.gz',
+    keywords = ['libraries', 'library', 'Gallica', 'France']
+    test_suite='tests',
+    classifiers = [],
+    install_requires = [ 'requests', 'urllib', 'xmltodict']
+)
+
