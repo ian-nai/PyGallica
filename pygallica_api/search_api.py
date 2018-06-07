d8bae3e8484601ee73db5f93ee819335fe43dbd8
Ian <iangoodale@utexas.edu>
2018-06-07 19:50:56 +0000

    Update search_api.py

--- pygallica_api/search_api.py
+++ pygallica_api/search_api.py
@@ -28,33 +28,3 @@ class Search(object):
         with open('gallica.xml') as xml:
             doc = xmltodict.parse(xml.read())
             return doc
-import urllib
-import xmltodict
-
-# Full documentation for this API can be found on Gallica's site: http://api.bnf.fr/api-gallica-de-recherche
-
-
-class Search(object):
-    
-    @staticmethod
-    def search(startRecord, *args):
-    
-    #This function passes your queries, separated by commas, in addition to the record you'd like to start with. 
-    
-        RECHERCHE_BASEURL = 'http://gallica.bnf.fr/SRU?operation=searchRetrieve&version=1.2&query=(gallica all '
-        START_REC = ')&startRecord='
-    
-        for arg in args:
-            search_string = (', '.join('"' + item + '"' for item in args))
-      
-        url = "".join([RECHERCHE_BASEURL, search_string, START_REC, startRecord])
-        print url
-
-        s = urllib.urlopen(url)
-        contents = s.read()
-        file = open('gallica.xml', 'w')
-        file.write(contents)
-        file.close()
-        with open('gallica.xml') as xml:
-            doc = xmltodict.parse(xml.read())
-            return doc
